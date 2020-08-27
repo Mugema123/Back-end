48aea9ade40197a311ca19a69c769e4795b9c25d
Mugema123 <mugemaleo@gmail.com>
2020-08-28 00:35:15 +0300

    'fetch-to-frontend'

--- models/news.js
+++ models/news.js
@@ -1,29 +1,20 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
   title: {
     type: String,
-    required: true
   },
-  Writter: { 
+  Writter: {
     type: String,
-    required: true
   },
   date: {
     type: Date,
-    required: true,
-    default: Date.now
+
+    default: Date.now,
   },
   content: {
     type: String,
-    required: true,
-    
-  }
+  },
 });
-
-
-
-
-
 
 module.exports = mongoose.model("news", schema);
